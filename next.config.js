214c836f031e26347ef88483292e620c457932bc
divyanshu patel <divyanshupatel15243@gmail.com>
2026-02-02 03:44:08 +0530

    Fix Next.js build by removing API call from _document

--- next.config.js
+++ next.config.js
@@ -15,42 +15,11 @@ const withPWA = require("next-pwa")({
 module.exports = withPWA({
   reactStrictMode: true,
   turbopack: {},
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
+  output: 'standalone',
   i18n: {
-    // These are all the locales you want to support in
-    // your application
     locales: ["en", "es", "fr", "de"],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
     defaultLocale: "en",
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
   },
-
-  // images: {
-  //   domains: [
-  //     "images.unsplash.com",
-  //     "img.icons8.com",
-  //     "i.ibb.co",
-  //     "i.postimg.cc",
-  //     "fakestoreapi.com",
-  //     "res.cloudinary.com",
-  //     "lh3.googleusercontent.com",
-  //     "res.cloudinary.com",
-  //     "lh3.googleusercontent.com",
-  //     "",
-  //     "images.dashter.com",
-  //   ],
-  // },
   images: {
     remotePatterns: [
       {
@@ -59,12 +28,9 @@ module.exports = withPWA({
       },
     ],
   },
-
+  // Disable static generation for auth and user pages
+  async generateBuildId() {
+    return 'build-' + Date.now();
+  },
   ...nextTranslate(),
 });
-
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-
-// module.exports = withBundleAnalyzer({});
